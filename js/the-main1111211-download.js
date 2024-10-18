a432471c5f78a2f3bd92dd9a2adef3f57f594dd7
Vipul Kumar <94109817+kartos8218@users.noreply.github.com>
2024-10-18 23:32:02 +0530

    Update the-main1111211-download.js

--- js/the-main1111211-download.js
+++ js/the-main1111211-download.js
@@ -195,9 +195,6 @@ function handleSuccessResponse(data, inputUrl) {
         // Construct video HTML
        const videoHtml = `
   <img style='background: black url(${thumbnailUrl}) center center/cover no-repeat; width:220x; height:auto; border-radius:17px;aspect-ratio:9/16;'/>`;
-
-// Insert the video into your desired element
-document.getElementById('thumb').innerHTML = videoHtml;
         const descriptionHtml = videoData.description ? `<h4><details><summary>View Description</summary>${sanitizeContent(videoData.description)}</details></h4>` : "";
         const durationHtml = videoData.size ? `<h5>${sanitizeContent(videoData.size)}</h5>` : "";
 
